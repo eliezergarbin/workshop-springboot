aca5d5f8066c48ada5f84d1819436684720fa59e
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-11-01 16:43:03 -0300

    adicionando testes, entidades e seedings

---
+++ workshop/src/main/java/com/example/workshop/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.workshop.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+}
